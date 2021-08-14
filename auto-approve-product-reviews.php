6f16d91ed39576d4fcb816f3b3b849d143acabf7
Andrei <andrei@laye.red>
2021-08-14 18:10:48 +0200

    Added - v1 of the plugin

---
+++ auto-approve-product-reviews.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: Auto Approve Product Reviews
+Plugin URI: https://layered.store/plugins/auto-approve-product-reviews
+Description: Auto approve product reviews with a minimum rating chosen by you
+Version: 1.0.0
+Author: Layered
+Author URI: https://layered.store
+License: GPL-3.0-or-later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+
+class LayeredAutoApproveReviews {
+
+	public static function start() {
+		return new static;
+	}
+
+	public function __construct() {
+
+		// Display settings to change auto approve options
+		add_filter('woocommerce_get_settings_products', [$this, 'settings'], 10, 2);
+
+		// Mark review as approved on submit
+		add_filter('pre_comment_approved', [$this, 'checkReview'], 500, 2);
+
+		// Display a shortcut Settings link on Plugin line
+		add_filter('plugin_action_links_auto-approve-product-reviews/auto-approve-product-reviews.php', [$this, 'actionLinks']);
+
+	}
+
+	public function settings(array $settings, string $current_section) {
+
+		if (!$current_section || $current_section === 'general') {
+
+			// Basic detection for Reviews section end
+			$productRatingEnd = count($settings) - 1;
+
+			// Search the position of Reviews section end in Settings
+			foreach ($settings as $index => $setting) {
+				if ($setting['type'] === 'sectionend' && $setting['id'] === 'product_rating_options') {
+					$productRatingEnd = $index;
+				}
+			}
+
+			array_splice($settings, $productRatingEnd, 0, [[
+				'title'		=>	'Auto approve rating',
+				'desc'		=>	'Auto approve reviews with this minimum rating',
+				'id'		=>	'woocommerce_reviews_auto_approve_rating',
+				'class'		=>	'wc-enhanced-select',
+				'default'	=>	'5',
+				'type'		=>	'select',
+				'options'	=> [
+					'0'	=>	'Do not automatically approve',
+					'1'	=>	'1',
+					'2'	=>	'2',
+					'3'	=>	'3',
+					'4'	=>	'4',
+					'5'	=>	'5',
+				],
+				'autoload'	=>	false,
+			]]);
+		}
+
+		return $settings;
+	}
+
+	public function checkReview($approved, $commentdata) {
+		$isUnapprovedReview = $commentdata['comment_type'] === 'review' && $approved == 0;
+
+		if ($isUnapprovedReview && isset($_POST['rating']) && ($minRating = get_option('woocommerce_reviews_auto_approve_rating')) && $_POST['rating'] >= $minRating) {
+			$approved = 1;
+		}
+
+		return $approved;
+	}
+
+	public function actionLinks(array $links) {
+		return array_merge([
+			'settings'	=>	'<a href="' . menu_page_url('wc-settings', false) . '&tab=products">Settings</a>'
+		], $links);
+	}
+
+	public static function onActivation() {
+		add_option('woocommerce_reviews_auto_approve_rating', 5);
+	}
+
+}
+
+
+// Run the plugin
+
+add_action('plugins_loaded', 'LayeredAutoApproveReviews::start');
+register_activation_hook(__FILE__, 'LayeredAutoApproveReviews::onActivation');
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Auto Approve Product reviews ===
+Contributors: andreiigna
+Tags: woocommerce, shop, products, reviews
+Requires at least: 5
+Tested up to: 5.8
+Requires PHP: 5.6
+Stable tag: trunk
+License: GPL-3.0-or-later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Auto approve product reviews with a minimum rating chosen by you
+
+== Description ==
+
+This is the plugin to use if your store gets so many reviews you don't have time to review them all!
+
+The plugin automatically approves reviews with 5 ⭐️, and has options to change the minimum rating that can be auto-approved.
+
+If the review is marked as spam by Akismet or another spam checking tool, the review will not be automatically approved. It will stay in queue for manual checking.
+
+== Screenshots ==
+
+1. Settings to change the minimum rating
+2. WooCommerce product reviews
+
+== Changelog ==
+
+= 1.0 - 17 May 2020 =
+* First release. Auto approves WooCommerce product reviews
